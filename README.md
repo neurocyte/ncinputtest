8124edfc6b5462bd300ec4c764faa29a0dc0e075
CJ van den Berg <cj@vdbonline.com>
2023-09-18 20:59:17 +0200

    Add source files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ncinputtest
+
 A simple notcurses input &amp; jitter tester written in Zig
+
+# Building
+
+```
+zig build run
+```
+
+Known to work with zig 0.12.0-dev.394+f2026e7dd
---
+++ build.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const notcurses_dep = b.dependency("notcurses", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "ncinputtest",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkLibrary(notcurses_dep.artifact("notcurses"));
+
+    b.installArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
